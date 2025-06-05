6221fcccb340bfac36e359c4ccfdbae6f9098192
dcarianela <aki.hayakawa5577@gmail.com>
2025-06-05 11:58:16 -0300

    exemplo concatenação

---
+++ 02-lidando-com-string.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const nome = "Sunoo";
+const sobrenome = "Kim"
+let idade = 21;
+let pais = "South Korea";
+let cidade = "Gyeonggi";
+
+// CONCATENAÇÃO tradicional usa o operador +
+let mensagem1 = "Meu nome é "+nome+" "+sobrenome+", tenho "+idade+" anos e moro em "+pais+"/"+cidade+".";
+
+console.log(mensagem1);
+
+// TEMPLATE STRING/LITERAL
