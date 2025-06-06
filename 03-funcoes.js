23cd4da695f7b559e68da6323423aa6b861933f9
dcarianela <aki.hayakawa5577@gmail.com>
2025-06-06 11:09:19 -0300

    relembra funções

--- 03-funcoes.js
+++ 03-funcoes.js
@@ -7,3 +7,57 @@ function exemplo1() {
 }
 
 exemplo1(); // chamada da função
+
+function separador(){
+    console.log("\n*******************************\n");
+}
+
+separador();
+
+// Chamada aqui dá erro, pois não pode ser feita antes da declaração
+// exemplo2(); 
+
+/* Sintaxe tradicional, função anônima
+Nesta sintaxe, a ordem de declaração IMPORTA. */
+const exemplo2 = function(){
+    console.log("Função anônima");
+};
+
+exemplo2(); // chamada (sempre após declarar a função anônima)
+
+separador();
+
+/* 1) Crie uma função (qualquer sintaxe que receba dois valores numéricos como parâmetros, calcule a diferença entre valores e retorne o resultado calculado) */
+function calcularDiferenca( valor1, valor2 ){
+    let diferenca = valor1 - valor2;
+    return diferenca;
+    return valor1 - valor2;
+}
+
+/* 2) Chame esta função pelo menos duas vezes passando valores diferentes e guardando os resultados em variáveis de escopo global. */
+let resultado1 = calcularDiferenca(10, 125);
+let resultado2 = calcularDiferenca(1250.7, -12);
+
+/* 3) Apresente os resultados no console. */
+console.log(resultado1);
+console.log(resultado2);
+
+
+separador();
+
+
+/* Sintaxe moderna, função flecha/seta (arrow function)
+Nesta sintaxe, a ordem de declaração IMPORTA. */
+const exemplo3 = () => {
+    console.log("Arrow Function");
+};
+
+exemplo3(); // Chamada deve ser feita após a declaração
+
+const saudacao = (nome) => {
+    console.log("Olá "+nome);   
+}
+
+saudacao("Sunoo");
+saudacao("Sunghoon");
+saudacao("Jaehyun");
