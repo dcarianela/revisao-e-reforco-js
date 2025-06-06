eaf32505684763ff348de54908de4d706e9b2b1f
dcarianela <aki.hayakawa5577@gmail.com>
2025-06-06 09:00:05 -0300

    relembra template string

--- 02-lidando-com-string.js
+++ 02-lidando-com-string.js
@@ -7,8 +7,16 @@ let pais = "South Korea";
 let cidade = "Gyeonggi";
 
 // CONCATENAÇÃO tradicional usa o operador +
-let mensagem1 = "Meu nome é "+nome+" "+sobrenome+", tenho "+idade+" anos e moro em "+pais+"/"+cidade+".";
+// let mensagem1 = "Meu nome é "+nome+" "+sobrenome+", tenho "+idade+" anos e moro em "+pais+"/"+cidade+".";
+
+let mensagem1 = "Meu nome é "+nome;
+mensagem1 += " "+sobrenome+", tenho "+idade;
+mensagem1 += " anos e moro em "+pais;
+mensagem1 += "/"+cidade+".";
 
 console.log(mensagem1);
 
 // TEMPLATE STRING/LITERAL
+let mensagem2 = `Meu nome é ${nome} ${sobrenome}, tenho ${idade} anos e moro em ${pais}/${cidade}. Sou ${idade >= 18 ? 'maior' : 'menor'} de idade.`;
+
+console.log(mensagem2);
