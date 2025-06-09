2ce69cd5a306eaaaecaa2e615b599336daaedad3
dcarianela <aki.hayakawa5577@gmail.com>
2025-06-09 09:34:45 -0300

    exporta com default

---
+++ modulos-esm/alunos.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const alunos = ["Sunoo", "Jake", "Terry", "Tae", "Gyu"];
+
+// Exportando com default(padrão), útil para recurso único (nãoo precisa de objeto).
+export default alunos;
